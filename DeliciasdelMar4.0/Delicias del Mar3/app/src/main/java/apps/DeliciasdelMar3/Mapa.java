2fb71806f11174496a88291bd0afca8ec7e01505
EusebioBuelvasmt <91988619+EusebioBuelvasmt@users.noreply.github.com>
2021-12-19 07:56:13 -0500

    Reto4 + apk

---
+++ DeliciasdelMar4.0/Delicias del Mar3/app/src/main/java/apps/DeliciasdelMar3/Mapa.java
@@ -0,0 +1,91 @@
+package apps.DeliciasdelMar3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.library.BuildConfig;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
+import apps.DeliciasdelMar3.proyectsqlite.R;
+
+public class Mapa extends AppCompatActivity {
+
+
+    ArrayList<OverlayItem> puntos = new ArrayList<OverlayItem>();
+    private MapView myOpenMapView;
+    private MapController myMapController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_mapa);
+        /* ---- necesitamos establecer el valor del agente de usuario en la configuración ------- */
+        //pre 5.6
+        //OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
+        //5.6 and newer
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        /* -------------------------------------------------------------------------------------- */
+
+        /*   punto de geolocalizacion de ejemplo */
+        GeoPoint Cartagena = new GeoPoint(10.533648216495678, -75.3992705301164);
+        GeoPoint Cartagena1= new GeoPoint(10.4260702, -75.5484499);
+        GeoPoint Bogota = new GeoPoint( 4.670621, -74.0555);
+        //GeoPoint Madrid = new GeoPoint(40.416775, -3.70379);
+        //GeoPoint Sydney = new GeoPoint(5.71666667, -72.92083333);
+        //----------------------------------------------------------------------------------
+
+        myOpenMapView = (MapView) findViewById(R.id.openmapview);
+        myOpenMapView.setBuiltInZoomControls(true);
+
+        myMapController = (MapController) myOpenMapView.getController();
+        myMapController.setCenter(Bogota);
+        myMapController.setZoom(5);
+
+        myOpenMapView.setMultiTouchControls(true);
+        puntos.add(new OverlayItem("Cartagena", "Centro Historico", Cartagena));
+        puntos.add(new OverlayItem("Cartagena1", "Calle Segunda de Badillo", Cartagena1));
+        puntos.add(new OverlayItem("Bogota", "Zona T", Bogota));
+        refrescaPuntos();
+    }
+
+
+
+
+    ItemizedIconOverlay.OnItemGestureListener<OverlayItem> tap = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+        @Override
+        public boolean onItemLongPress(int arg0, OverlayItem arg1) {
+            return false;
+        }
+        @Override
+        public boolean onItemSingleTapUp(int index, OverlayItem item) {
+            return true;
+        }
+    };
+    private void refrescaPuntos() {
+        myOpenMapView.getOverlays().clear();
+        ItemizedIconOverlay.OnItemGestureListener<OverlayItem> tap = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemLongPress(int arg0, OverlayItem arg1) {
+                return false;
+            }
+
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return true;
+            }
+        };
+
+        ItemizedOverlayWithFocus<OverlayItem> capa = new ItemizedOverlayWithFocus<>(this, puntos, tap);
+        capa.setFocusItemsOnTap(true);
+        myOpenMapView.getOverlays().add(capa);
+    }
+}
